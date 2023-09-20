2b463a782e58ef1f45fcbc5be2253cdee5a51cf8
yinlean <yinlean@bytedance.com>
2023-09-20 20:15:39 +0800

    feat: 历史告警

--- src/pages/HistoryAlarm/index.tsx
+++ src/pages/HistoryAlarm/index.tsx
@@ -1,7 +1,7 @@
-import { getHistoryAlert } from '@/api/alert';
+import { getBizInfo, getHistoryAlert } from '@/api/alert';
 import { AppstoreOutlined } from '@ant-design/icons';
 import { Badge, Form, Input, Select, Table, Tag } from 'antd';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 const columns = [
   {
@@ -35,6 +35,19 @@ const columns = [
 ];
 
 const HistoryAlarm = () => {
+  const [teamList, setTeamLIst] = useState<any[]>([]);
+
+  const getTeamList = async () => {
+    const res = await getBizInfo({
+      page: 1,
+      onePage: 100,
+    });
+    setTeamLIst(res.query ?? []);
+  };
+  useEffect(() => {
+    getTeamList();
+  }, []);
+
   // 获取历史告警列表
   const getHistoryList = async () => {
     // if (!bizID) return;
@@ -53,31 +66,52 @@ const HistoryAlarm = () => {
     <div>
       <Form
         layout="inline"
-        initialValues={{ remember: true }}
+        initialValues={{}}
         // onFinish={onFinish}
       >
         <Form.Item label="" name="username">
           <AppstoreOutlined style={{ fontSize: 20 }} />
           {/* <BarsOutlined style={{ fontSize: 20 }} /> */}
         </Form.Item>
-        <Form.Item label="" name="username">
-          <Select style={{ width: 100 }} placeholder="时间">
-            <Select.Option value="demo">最近6小时</Select.Option>
+        <Form.Item label="时间" name="startTime">
+          <Select style={{ width: 200 }} placeholder="时间">
+            <Select.Option value={Date.now() - 6 * 60 * 60 * 1000}>
+              最近6小时
+            </Select.Option>
+            <Select.Option value={Date.now() - 24 * 60 * 60 * 1000}>
+              最近24小时
+            </Select.Option>
+            <Select.Option value={Date.now() - 3 * 24 * 60 * 60 * 1000}>
+              最近3天
+            </Select.Option>
+            <Select.Option value={Date.now() - 7 * 24 * 60 * 60 * 1000}>
+              最近7天
+            </Select.Option>
           </Select>
         </Form.Item>
-
-        <Form.Item label="" name="c">
-          <Input placeholder="集群" />
+        <Form.Item label="" name="name">
+          <Input placeholder="" />
         </Form.Item>
-
         <Form.Item name="remember">
           <Select placeholder="业务组" style={{ width: 140 }}>
-            <Select.Option>最近6小时</Select.Option>
+            {teamList?.map((v) => (
+              <Select.Option value={v.id} key={v.id}>
+                {v.name}
+              </Select.Option>
+            ))}
           </Select>
         </Form.Item>
-        <Form.Item label="" name="a">
+        <Form.Item label="事件级别" name="severity">
           <Select placeholder="事件级别">
-            <Select.Option>最近6小时</Select.Option>
+            <Select.Option value={1}>一级</Select.Option>
+            <Select.Option value={3}>二级</Select.Option>
+            <Select.Option value={3}>三级</Select.Option>
+          </Select>
+        </Form.Item>
+        <Form.Item label="状态" name="state">
+          <Select placeholder="事件级别">
+            <Select.Option value={'true'}>告警</Select.Option>
+            <Select.Option value={'false'}>恢复</Select.Option>
           </Select>
         </Form.Item>
 
